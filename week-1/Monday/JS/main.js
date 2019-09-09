02765f74f419d67340545ef6ca873953060f8064
prettyleka <54413358+prettyleka@users.noreply.github.com>
2019-09-09 10:00:59 +0300

    Add files via upload
    Monday Exercises

---
+++ week-1/Monday/JS/main.js
@@ -0,0 +1,20 @@
+const password = 2704 
+const confirmPassword =2704
+
+console.log(password == confirmPassword)
+
+
+
+let a = 3
+a = 4
+let c = 0
+let b = a
+b = 2
+a = b
+b = c
+c = a
+a = b
+
+console.log(a)
+console.log(b)
+console.log(c)
